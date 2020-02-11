a26542eae074df5a2202847dc3a88dbc3f40993b
Lucas Yang <lucas.yang@thomsonreuters.com>
2020-02-11 11:25:52 -0500

    List data rendering

---
+++ src/components/NameList.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import Person from './Person'
+
+
+function NameList(){
+
+    const persons = [
+        {
+            id: 1,
+            name: "Kevin",
+            age: 30,
+            skill: 'React'
+        },
+        {
+            id: 2,
+            name: "Mike",
+            age: 25,
+            skill: 'Angular'
+        },
+        {
+            id: 3,
+            name: "Louisa",
+            age: 32,
+            skill: 'C++'
+        },
+        {
+            id: 4,
+            name: "Unknown",
+            age: 42,
+            skill: 'Full stack'
+        }
+    ];
+
+
+    const personList = persons.map( person => <Person person={person}/>);
+    return (
+        <div>
+            {personList}
+        </div>
+    );
+};
+
+export default NameList;
