8a56fc857c8f691e3b02ab6a3c473b2ba414c8f7
Lucas Yang <lucas.yang@thomsonreuters.com>
2020-02-11 18:03:54 -0500

    Component Updating Lifecycle methods

--- src/components/LifecycleA.js
+++ src/components/LifecycleA.js
@@ -22,11 +22,32 @@ class LifecycleA extends Component{
         console.log("LifecycleA componentDidMount");
     }
 
+    shouldComponentUpdate(){
+        console.log("LifecycleA shouldComponentUpdate");
+        return true;
+    }
+
+    getSnapshotBeforeUpdate(prevProps, prevState){
+        console.log("LifecycleA getSnapshotBeforeUpdate");   
+        return null;
+    }
+
+    componentDidUpdate(){
+        console.log("LifecycleA componentDidUpdate");
+    }
+
+    changeState = ()=>{
+        this.setState({
+            name: "Good Study"
+        })
+    }
+
     render(){
         console.log("LifecycleA render");
         return (
             <div>
                <div>LifecycleA</div> 
+               <button onClick={this.changeState}>Change State</button>
                <LifecycleB />
             </div>
         );
